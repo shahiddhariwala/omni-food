154fd0f8d027f0e614d98d8a3fbaef41ca9f49ed
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-28 15:03:20 +0530

    Built  Cool Responsive Nav bar

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,27 +1,45 @@
 $(document).ready(function () {
-    // for sticky nav
-    var waypoints = $('.js--section-features').waypoint(function(direction) {
-        if(direction=="down")
-        {
-            $('nav').addClass('sticky');
-        }
-        else{
-            $('nav').removeClass('sticky');
-        }
-      }, {
-        offset: '60px;'
-      });
+  // for sticky nav
+  var waypoints = $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;",
+    }
+  );
 
-      // Scroll effect for button
-      $('.js--scroll-to-plans').click(function()
-      {
-          console.log("imhere");
-            $('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
-      });
-      $('.js--scroll-to-start').click(function()
-      {
-            $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);
-      });
+  // Scroll effect for button
+  $(".js--scroll-to-plans").click(function () {
+    $("html,body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+  $(".js--scroll-to-start").click(function () {
+    $("html,body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
+  });
 
+  /* Mobile navigation */
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon i");
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+  });
 });
-
