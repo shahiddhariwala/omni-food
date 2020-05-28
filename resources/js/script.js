d35f2f38f47d735e0f7d575e3dc49355e18c3045
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-28 16:03:16 +0530

    Added cool animations while scrolling using animate.css and waypoints

--- resources/js/script.js
+++ resources/js/script.js
@@ -27,6 +27,43 @@ $(document).ready(function () {
     );
   });
 
+  /* Animations on scroll */
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass(" animate__animated  animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass(" animate__animated  animate__fadeInUp ");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass(" animate__animated  animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass(" animate__animated  animate__pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
   /* Mobile navigation */
   $(".js--nav-icon").click(function () {
     var nav = $(".js--main-nav");
